64c56299973a24485d36a8f71074c77233adfcd1
muamarzidan <zidanprofix93@gmail.com>
2022-07-29 06:55:08 +0700

    faqs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
+function calculate(value) {
+    return value < 2 ? value : (calculate(value - 1) + calculate(value - 2));
+}
+
+console.log(calculate(3));
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
