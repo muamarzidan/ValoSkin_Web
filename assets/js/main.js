19310ad7ef7b455c20eb4afae14e220cfc9d2fc5
muamarzidan <zidanprofix93@gmail.com>
2022-07-29 11:06:04 +0700

    color

--- assets/js/main.js
+++ assets/js/main.js
@@ -140,6 +140,9 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.home__data`)
+sr.reveal(`.steps__title`)
+sr.reveal('.section__title-center', {origin: 'right'})
+sr.reveal('.product__description', {origin: 'left'})
 sr.reveal(`.home__img`, {delay: 500})
 sr.reveal(`.home__social`, {delay: 600})
 sr.reveal(`.about__img, .contact__box`,{origin: 'left'})
